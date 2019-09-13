7b1355ddeef7cdbed8c02ac240e6b7e7bbc1a0f3
Allan Reid <allanreid@Allans-MBP.hsd1.ca.comcast.net>
2019-09-13 16:20:12 -0700

    building app

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -1,11 +1,13 @@
 <template>
   <q-item
-    @click="task.completed = !task.completed"
+    @click="updateTask({ id: id, updates: { completed: !task.completed } })"
     :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
     clickable
     v-ripple>
     <q-item-section side top>
-      <q-checkbox v-model="task.completed" />
+      <q-checkbox 
+      :value="task.completed" 
+      class="no-pointer-events" />
     </q-item-section>
 
     <q-item-section>
@@ -37,13 +39,38 @@
         </div>
       </div>
     </q-item-section>
+
+    <q-item-section side>
+      <q-btn
+      @click.stop="promptToDelete(id)"
+       flat
+       round
+       dense 
+       color="red" 
+       icon="delete" />
+    </q-item-section>
+
   </q-item>
-      
 </template>
 
 <script>
+  import { mapActions } from 'vuex'
+
   export default {
-    props: ['task', 'id']
+    props: ['task', 'id'],
+    methods: {
+      ...mapActions('tasks', ['updateTask']),
+      promptToDelete(id) {
+        this.$q.dialog({
+          title: 'Confirm',
+          message: 'Really delete?',
+          cancel: true,
+          persistent: true
+        }).onOk(() => {
+          console.log('deleted')
+        })
+      }
+    }
   }
 </script>
 
